86d4e50b08e52666a553447b215905644005ad84
Kathryn Clark <kathrynlael@gmail.com>
2020-03-04 22:49:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,10 +19,11 @@ def pop_with_args(number_of_elements)
 end
 
 def using_shift(array)
-  
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  my_favorite_cities.shift
 end 
   
-
+def 
 
 
 
