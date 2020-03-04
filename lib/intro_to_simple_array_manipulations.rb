bb9fc9a7fbfb01a7b62cbb7c67bb6260a1f39157
Kathryn Clark <kathrynlael@gmail.com>
2020-03-04 23:27:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,8 +37,7 @@ end
 def using_insert(array,element)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   another_language = "Python"
-  new_array = using_insert(list_of_programming_languages, "Python")
-  new_array[4]= "Python"
+  new_array[4] = (list_of_programming_languages, "Python")
 end
   
   
