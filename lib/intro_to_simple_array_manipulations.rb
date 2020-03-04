cb5e35ce24128badafd2bf8d1fc4f2a6284fdacd
Kathryn Clark <kathrynlael@gmail.com>
2020-03-04 22:07:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array,string)
+   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  updated_array = colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(array,string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  updated_array = bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop(array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  new_continents=continents.pop
+  continents.size 
+  new_continents
+end
+
+def pop_with_args(number_of_elements)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  dog_breeds.pop(2)
+end
+  
+
+
+
+
+
+  
