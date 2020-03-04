2c9482413808018c74bedbbaa893811f0dc5bcef
Kathryn Clark <kathrynlael@gmail.com>
2020-03-04 23:16:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,7 +31,6 @@ end
 def using_concat(array1,array2)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  my_favorite_things.concat(more_favs)
   all_my_favs=my_favorite_things.concat(more_favs)
   
 end
