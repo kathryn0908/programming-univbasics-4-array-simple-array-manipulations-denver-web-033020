6bd67c84e9ba8e022f61ca1f637f2cbcd48c81c0
Kathryn Clark <kathrynlael@gmail.com>
2020-03-04 23:01:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,7 +26,10 @@ end
 def shift_with_args(array)
   ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
   brands_removed=ice_cream_brands.shift(2)
-end 
+end
+
+def using_concat(array1,array2)
+  
   
 
 
