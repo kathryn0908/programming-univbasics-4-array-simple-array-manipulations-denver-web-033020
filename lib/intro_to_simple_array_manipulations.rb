47cb7ff340cac971004fb90f88dc58a5e36d108c
Kathryn Clark <kathrynlael@gmail.com>
2020-03-04 23:22:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,7 +37,7 @@ end
 def using_insert(array,element)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
     another_language = "Python"
- list_of_programming_languages[4]= another_language
+ list_of_programming_languages[4]= "Python"
 end
   
   
