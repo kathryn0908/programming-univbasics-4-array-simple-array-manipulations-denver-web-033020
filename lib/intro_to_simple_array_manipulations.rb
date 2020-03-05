644c72b811e6648c81ad47f893b7e7ff5ad3df14
Kathryn Clark <kathrynlael@gmail.com>
2020-03-05 00:00:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -51,6 +51,8 @@ def using_flatten(instruments)
 end 
 
 def using_delete(array, string)
+   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+    no_offense_steven = (instructors, "Steven")
   
   
   
