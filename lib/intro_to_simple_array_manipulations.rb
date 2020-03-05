9d30ea411c85b62de2e115d020f71e5fb559f780
Kathryn Clark <kathrynlael@gmail.com>
2020-03-05 00:02:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,7 +52,8 @@ end
 
 def using_delete(array, string)
    instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-    no_offense_steven = (instructors, "Steven")
+    no_offense_steven = instructors.delete_at(1)
+end
   
   
   
